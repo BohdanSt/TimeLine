d60632d7f0c5879cfbc5d41cfef6ae35e31013cb
Leonid <leonardodestakhov@gmail.com>
2017-06-29 20:53:13 +0300

    Add class Life

---
+++ TimeLine/Life.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimeLine
+{
+    /// <summary>
+    /// Логика взаимодействия для Life.xaml
+    /// </summary>
+    public partial class Life : UserControl
+    {
+        public int AmountOfLife { get; set; }
+        List<LifeControl> lifelist = new List<LifeControl>();
+        public Life()
+        {
+            InitializeComponent();
+            for(int i = 0; i < AmountOfLife; i++)
+            {
+                LifeContainer.ColumnDefinitions.Add(new ColumnDefinition());
+                LifeControl control = new LifeControl();
+                Grid.SetColumn(control, i);
+                LifeContainer.Children.Add(control);
+                lifelist.Add(control);
+            }
+        }
+    }
+}
---
+++ TimeLine/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeLine
+{
+    class Question
+    {
+
+        public string Name { get; set; }
+        public int Index { get; set; }
+
+    }
+}
