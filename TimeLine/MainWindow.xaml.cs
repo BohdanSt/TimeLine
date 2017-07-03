e4516d51c3d7355a93a176d49b1586ffaaeae051
Leonid <leonardodestakhov@gmail.com>
2017-07-03 21:44:07 +0300

    Add class Question

--- TimeLine/MainWindow.xaml.cs
+++ TimeLine/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace TimeLine
         public MainWindow()
         {
             InitializeComponent();
+            Question.ReadQuestions();
         }
     }
 }
--- TimeLine/Question.cs
+++ TimeLine/Question.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TimeLine
 {
@@ -11,6 +12,19 @@ namespace TimeLine
 
         public string Name { get; set; }
         public int Index { get; set; }
+
+        public static List<Question> ReadQuestions()
+        {
+            int i = 0;
+            List<Question> Qst = new List<Question>();
+            string[] readText = File.ReadAllLines("Question.txt");
+            foreach (string s in readText)
+            {
+                i++;
+                Qst.Add(new Question() { Name = s, Index = i });   
+            }
+            return Qst;
+        }
 
     }
 }
